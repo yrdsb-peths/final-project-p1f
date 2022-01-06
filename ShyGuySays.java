feb2817fdf0ebb5d28468d3858da6c60dc958ae2
Kevin_Qwang <335744355@gapps.yrdsb.ca>
2022-01-06 11:55:09 -0500

    some assets for shyguy says
    some assets for shyguy says

---
+++ ShyGuySays.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ShyGuySays here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShyGuySays extends World
+{
+
+    /**
+     * Constructor for objects of class ShyGuySays.
+     * 
+     */
+    public ShyGuySays()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+}
