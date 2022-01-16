765bb0ddea85f0984ef8731ac769bf4f80905164
Kevin_Qwang <335744355@gapps.yrdsb.ca>
2022-01-16 11:37:53 -0500

    Simon Says Works
    Simon Says audio bug fixed
    Only single player though

--- FlagMan.java
+++ FlagMan.java
@@ -8,18 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class FlagMan extends Actor
 {
-    //too many errors, starting over
     GreenfootImage lFlagMan = new GreenfootImage("flaggerL.png");
     GreenfootImage rFlagMan = new GreenfootImage("flaggerR.png");
-    
-    GreenfootSound main = new GreenfootSound("Mario Party (Music) - Dodging Danger.mp3");
     
     private SimpleTimer timer = new SimpleTimer();
     public FlagMan(){
         lFlagMan.scale(80, 80);
         rFlagMan.scale(80, 80);
-        timer.mark();
-        main.setVolume(30);
+        timer.mark();    
     }
     
     public void act(){
@@ -43,14 +39,11 @@ public class FlagMan extends Actor
             }
         }
         
-        if((timer.millisElapsed() > 62500) || !PlayerSays.alive){
-            main.stop();
+        if((timer.millisElapsed() > 60000) || !PlayerSays.alive){
             GameEnded done = new GameEnded();
             Greenfoot.setWorld(done);
             System.out.println(timer.millisElapsed());
-        }
-        else{
-            main.play();
+            timer.mark();
         }
     }
 }
--- ShyGuySays.java
+++ ShyGuySays.java
@@ -8,15 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class ShyGuySays extends World
 {
     
-    static PlayerSays player = new PlayerSays();
-    static FlagManSign flagSign = new FlagManSign();
-    static FlagMan flagMan = new FlagMan();
+    private static PlayerSays player = new PlayerSays();
+    private static FlagManSign flagSign = new FlagManSign();
+    private static FlagMan flagMan = new FlagMan();
     
     //initializing the player
     GreenfootImage idle = new GreenfootImage("WalkF1.png");
     GreenfootImage unknownSign = new GreenfootImage("uniFlag.png");
     GreenfootImage leftFlagMan = new GreenfootImage("flaggerL.png");
     
+    GreenfootSound main = new GreenfootSound("Mario Party (Music) - Dodging Danger.mp3");
     /**
      * Constructor for objects of class ShyGuySays.
      * 
@@ -26,6 +27,8 @@ public class ShyGuySays extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1);
         
+        main.setVolume(30);
+
         leftFlagMan.scale(80, 80);
         flagMan.setImage(leftFlagMan);
         addObject(flagMan, 200, 480);
@@ -41,6 +44,11 @@ public class ShyGuySays extends World
     }
     
     public void act(){
-        //3...2...1...start!
+        if(PlayerSays.alive){
+            main.play();
+        }
+        else{
+            main.stop();
+        }
     }
 }
