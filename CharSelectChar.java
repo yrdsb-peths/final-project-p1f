8d6254d73338a4f6afcdd63ce21f8dcf1c2bfa7f
Kevin_Qwang <335744355@gapps.yrdsb.ca>
2022-01-17 11:39:49 -0500

    some basic stuff
    some basic stuff

---
+++ CharSelectChar.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CharSelectChars here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CharSelectChar extends Actor
+{
+    private static int numClicks;
+    private static String playerChoice;
+    private String thisName;
+    public CharSelectChar(String player){
+        GreenfootImage waitToPick = new GreenfootImage(player + "/menu.png");
+        if(player.equals("LuigiMenu")){
+            waitToPick.scale(150, 150);
+        }
+        else if(player.equals("MarioMenu")){
+            waitToPick.scale(90, 134);
+        }
+        else{
+            waitToPick.scale(70, 132);
+        }
+        setImage(waitToPick);
+        CharSelectChar.playerChoice = "";
+        thisName = player;
+    }
+    
+    /**
+     * Act - do whatever the CharSelectChars wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.mousePressed(this)){
+            playerChoice = thisName;
+            System.out.println(playerChoice);
+            numClicks++;
+        }
+    }   
+    
+    public static int getNumClicks(){
+        return CharSelectChar.numClicks;
+    }
+    public static String getPlayerChoice(){
+        return CharSelectChar.playerChoice;
+    }
+}
---
+++ CharacterSelect.java
@@ -0,0 +1,51 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CharacterSelect here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CharacterSelect extends World
+{
+    
+    /**
+     * Constructor for objects of class CharacterSelect.
+     * 
+     */
+    public CharacterSelect()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        CharSelectChar mario = new CharSelectChar("MarioMenu");
+        CharSelectChar luigi = new CharSelectChar("LuigiMenu");
+        
+        CharSelectChar koopaG = new CharSelectChar("KoopaGreen");
+        CharSelectChar koopaP = new CharSelectChar("KoopaPurple");
+        CharSelectChar koopaR = new CharSelectChar("KoopaRed");
+        CharSelectChar koopaS = new CharSelectChar("KoopaSilver");
+        CharSelectChar koopaY = new CharSelectChar("KoopaYellow");
+        
+        addObject(mario, 200, 300);
+        addObject(luigi, 300, 305);
+        addObject(koopaG, 400, 300);
+        addObject(koopaP, 500, 300);
+        addObject(koopaR, 600, 300);
+        addObject(koopaS, 700, 300);
+        addObject(koopaY, 800, 300);
+    }
+    
+    public void act(){
+        System.out.println(String.valueOf(CharSelectChar.getNumClicks()));
+        System.out.println(CharSelectChar.getPlayerChoice());
+    }
+}
