2dd3cca48e4677dda99f76dfbca170b2fd951890
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2022-01-21 09:08:48 -0500

    Revert "Merge branch 'Dev'"
    This reverts commit fda753a47a5bfefab39482ae8ed20b834a308dbb, reversing
    changes made to b2eb7a8259a3d4d692f3d837efc60d7567f72301.

--- GameWorld1.java
+++ GameWorld1.java
@@ -1,60 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.ArrayList;
 
 /**
- * Animated game over screen - determines the winning player and allows user to re-play
+ * Write a description of class GameWorld1 here.
  * 
- * @author Victoria Zhang, Eric Zhang, Tanya Gu
- * @version January 2022
+ * @author (your name) 
+ * @version (a version number or a date)
  */
-public class GameOverScreen extends World
+public class GameWorld1 extends World
 {
-    private GreenfootImage[] bkg = new GreenfootImage[28];
-    private Button backToTitle;
+
     /**
-     * Constructor for objects of class GameOverScreen.
+     * Constructor for objects of class GameWorld1.
      * 
-     * @param playersRef Players of the game
      */
-    public GameOverScreen(ArrayList<MapCharacter> playersRef)
+    public GameWorld1()
     {    
-        // Create a new world with 1000x600 cells with a cell size of 1x1 pixels.
-        super(1000, 600, 1); 
-        
-        setBackground(new GreenfootImage("WinningScreen/16.png"));
-        for (int i = 0; i < bkg.length; i++) { //animated background
-            bkg[i] = new GreenfootImage("images/WinningScreen/" + (i) + ".png"); 
-        }
-        
-        MainSound.setSound(new GreenfootSound("EndTheme.mp3"));
-        MainSound.play();
-        
-        //determine the winning player
-        ArrayList<MapCharacter> players = new ArrayList<MapCharacter>(playersRef);
-        Utils.sort(players);
-        Player player = new Player(players.get(0).getName(), 5);
-        addObject(player,400,395);
-        addObject(new Label("Wins!",70),550,415);
-        
-        backToTitle = new Button("Play Again!", 0.7f);
-        addObject(backToTitle, 500, 550);
-    }
-    
-    /**
-     * Method that animates the background and checks if the 
-     * re-play button is clicked, if so, return to title screen
-     */
-    int imageIndex = 0, fr = 0;
-    public void act() {
-        if (fr == 5) {
-            setBackground(bkg[imageIndex]);
-            imageIndex = (imageIndex + 1) % bkg.length;
-            fr = 0;
-        }
-        fr++;
-        
-        if (Greenfoot.mouseClicked(backToTitle)) { 
-            Greenfoot.setWorld(new TitleScreen());
-        }
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
     }
 }
